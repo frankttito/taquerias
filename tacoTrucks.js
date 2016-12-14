da2f51744ac9cddb6b3a4ee9c90b2d2542ac8976
frankttito <frank.r.ttito@gmail.com>
2016-12-14 14:45:25 -0800

    Update tacoTrucks.js

--- tacoTrucks.js
+++ tacoTrucks.js
@@ -1,27 +1,16 @@
-//JavaScript File
-var api_key = 'keyjUyqxvapBphwo4';
-var ttrucks_record = 'https://api.airtable.com/v0/appSrgke7E0ElZhMY/TTrucks?api_key=' + api_key;
+var airtable_key = "keyjUyqxvapBphwo4";
+var gmaps_key = "AIzaSyCO9Bmu58rNIQbqlT0MxdUtkwKM6wCEDEU";
 
-function renderRecords(data) {
-  $(data.records).each(function(index, truck) {
-    var truck_name = truck.fields['Name'];
-    var truck_where = truck.fields['District'];
-    var truck_pics = truck.fields['Pictures'];
-    var truck_address = truck.fields['Address'];
-    var truck_info = '';
-    if (truck_name) {
-      truck_info += `<li>`;
-      if (truck_pics) {
-        $.each(truck_pics, function(i, pic) {
-          truck_info += `<img src="${pic.url}">`;
+function initMap() {
+        var sf = {lat: 37.7729371, lng: -122.422864};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: sf
         });
-      }
-      truck_info += `<br> Name: ${truck_name} <br> Where: ${truck_where}<br> Address: ${truck_address} <br>`;
-      //truck_info += `<br> Address: ${truck_address} <br>`;
-      truck_info += `</li>`;
-    }
-    $('.truck').append(truck_info);
-  });
-}
-
-$.get(ttrucks_record, renderRecords);
+        
+        var marker = new google.maps.Marker({
+          position: sf,
+          map: map
+        });
+        
+  }
