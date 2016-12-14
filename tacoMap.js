b1fb7d77dde2b4bccc1d409253a2f888afad05c7
frankttito <frank.r.ttito@gmail.com>
2016-12-14 14:47:56 -0800

    Update tacoMap.js

--- tacoMap.js
+++ tacoMap.js
@@ -1,56 +1,18 @@
 
+
+var airtable_key = "keyjUyqxvapBphwo4";
+var gmaps_key = "AIzaSyCO9Bmu58rNIQbqlT0MxdUtkwKM6wCEDEU";
+
 function initMap() {
-  //initialize map
-  var sf = {lat: 37.7598334, lng: -122.4441808};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: sf
-  });
-  
-  var places = [];
- 
-  $.get(loc_records, function(data) {
-    var letter = "A";
-    console.log(data.records);
-    $(data.records).each(function(i, loc){  
-      //add latitude/longitude to locations
-      places[i] = {lat: parseFloat(loc.fields["Latitude"]), lng: parseFloat(loc.fields["Longitude"])};
-      
-      //Append header letters per each section, 0th , i'th , and final element
-      if(i === 0) {
-        letter =  data.records[0].fields["Name"][0].toUpperCase();
-        $(".locations").append("<h1 class=\"letter\">" + letter + "</h1>");
-      }
-      
-      if(loc.fields["Name"][0].toUpperCase() === letter && i > 0) {
-         if(letter !== data.records[i-1].fields["Name"][0].toUpperCase()) {
-          $(".locations").append("<h1 class=\"letter\">" + letter + "</h1>");
-        }
-        
-  		  if(data.records[i+1]) {
-  		    letter = data.records[i+1].fields["Name"][0].toUpperCase();
-  		  }
-  	  }
-  	  
-  	  //setup each location
-  	  var html = "<div class=\"labels\" onclick=\"location.href='detail.html?id=" + loc.id + "';\" style=\"cursor: pointer;\">";
-  	  html += `<li class="description">${loc.fields["Name"]} <br>Location: ${loc.fields["Neighborhood"]}</li>`;
-  	  //html += "<img src=\"" + loc.fields["Picture of Landmark"]["0"].url + "\" width=\"auto\" height=\"400\" alt=\"picture\">";
-  	  html += "</div>";
-  	  $(".locations").append(html);
-  	
-      //draw markers onto map
-      var marker = new google.maps.Marker({
-        position: places[i],
-        map: map
-      });
-      
-      //Draw labels onto markers
-      infowindow = new google.maps.InfoWindow();
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent("<a href=\"detail.html?id=" + loc.id + "\">" + loc.fields["Name"] + "</a>");
-          infowindow.open(map, this);
+        var sf = {lat: 37.7729371, lng: -122.422864};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: sf
         });
-    }); 	 
-  });
-};
+        
+        var marker = new google.maps.Marker({
+          position: sf,
+          map: map
+        });
+        
+  }
